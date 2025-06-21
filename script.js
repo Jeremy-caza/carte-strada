a4818cbe2bf90102ebee455f205809532efdbc76
Jeremy-caza <caza.comm@gmail.com>
2025-06-21 17:00:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ document.getElementById('popup').style.display = 'flex';
 function closePopup() {
 document.getElementById('popup').style.display = 'none';
 }
+
+function toggleList(id) {
+const list = document.getElementById(id);
+if (!list) return;
+
+list.classList.toggle('open');
+}
