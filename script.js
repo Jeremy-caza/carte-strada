4471d3ae1ba92ef2c63f075b99678bc704fffec0
Jeremy-caza <caza.comm@gmail.com>
2025-06-22 15:45:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,18 @@ document.getElementById('popup').style.display = 'none';
 
 function toggleList(id) {
 const list = document.getElementById(id);
-if (!list) return;
 
-list.classList.toggle('open');
+if (list.classList.contains('open')) {
+list.style.maxHeight = '0';
+list.classList.remove('open');
+} else {
+const allLists = document.querySelectorAll('.liste');
+allLists.forEach((l) => {
+l.style.maxHeight = '0';
+l.classList.remove('open');
+});
+
+list.classList.add('open');
+list.style.maxHeight = list.scrollHeight + 'px';
+}
 }
