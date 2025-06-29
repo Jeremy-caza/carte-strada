5207dddcb73753e386cd759493f6065746057999
Jeremy-caza <caza.comm@gmail.com>
2025-06-29 20:12:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,22 +13,14 @@ const list = document.getElementById(id);
 if (list.classList.contains('open')) {
 list.style.maxHeight = '0';
 list.classList.remove('open');
-Element.classList.remove('active');
 } else {
 const allLists = document.querySelectorAll('.liste');
-const allcategories = document.querySelectorAll('.categorie');
-
 allLists.forEach((l) => {
 l.style.maxHeight = '0';
 l.classList.remove('open');
 });
 
-allcategories.forEach((c)=> {
-    c.classList.remove('active');
-})
-
 list.classList.add('open');
 list.style.maxHeight = list.scrollHeight + 'px';
-element.classList.add('active');
 }
 }
